06ed08e5b5e2561881ff7f533636d38cc03584fe
jihye <jyebe9034@gmail.com>
2020-07-04 17:06:22 +0900

    iamrich app

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Hannah's Mac on 2020/07/04.
+//  Copyright © 2020 Hannah Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
